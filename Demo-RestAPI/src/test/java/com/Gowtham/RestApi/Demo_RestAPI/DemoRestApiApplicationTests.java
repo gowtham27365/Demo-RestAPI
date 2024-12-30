dc16385e0aeee5c07a768ede28b3c23ab521f2bb
Gowtham kumar <gowtham27365@gmail.com>
2024-12-30 15:29:36 +0530

    Innitial project  setup

---
+++ Demo-RestAPI/src/test/java/com/Gowtham/RestApi/Demo_RestAPI/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gowtham.RestApi.Demo_RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
