3b721ac0db307ba5eb0a30f1711b6d435279ff20
Gowtham kumar <gowtham27365@gmail.com>
2024-12-30 20:14:15 +0530

    Added Rest Apis for Hello world

--- Demo-RestAPI/src/main/java/com/Gowtham/RestApi/Demo_RestAPI/helloworld/HelloWorldController.java
+++ Demo-RestAPI/src/main/java/com/Gowtham/RestApi/Demo_RestAPI/helloworld/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.Gowtham.RestApi.Demo_RestAPI.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
@@ -10,9 +10,20 @@ public class HelloWorldController {
 	
 	
     // /hello-world
-	@RequestMapping(method = RequestMethod.GET, path="/hello-world")
+	//@RequestMapping(method = RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("HelloWorld");
+	}
+	
+	@GetMapping(path = "/hello-world/pathVariable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World,%s", name));
 	}
 	
 }
